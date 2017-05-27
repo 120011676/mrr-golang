27abd7396445e300867d15ee954a7af12a25728c
Say.li <120011676@qq.com>
2017-05-27 16:48:36 +0800

    ‘增加接口时间字段错误提示’

--- controller/meet.go
+++ controller/meet.go
@@ -20,29 +20,48 @@ func Reserve(w http.ResponseWriter, r *http.Request) {
 	password := r.FormValue("password")
 	startDate := r.FormValue("startDate")
 	endDate := r.FormValue("endDate")
-	ed, _ := time.Parse("2006-01-02 15:04:05", endDate)
-	sd, _ := time.Parse("2006-01-02 15:04:05", startDate)
-	m := entity.Meet{
-		Id:             bson.NewObjectId(),
-		City:           city,
-		Floor:          floor,
-		Room:           room,
-		ConferenceName: conferenceName,
-		People:         people,
-		Phone:          phone,
-		Password:       password,
-		StartDate:      sd,
-		EndDate:        ed,
-		Status:         true,
-		CreateDate:     time.Now(),
-	}
-	e := service.Reserve(m)
 	result := entity.Result{Status: false, Date: time.Now().Format("2006-01-02 15:04:05")}
-	if e != nil {
-		result.Msg = e.Error()
+	if startDate == "" {
+		result.Code = -1
+		result.Msg = "预约开始时间不能为空"
+	} else if endDate == "" {
+		result.Code = -2
+		result.Msg = "预约结束时间不能为空"
 	} else {
-		result.Status = true
-		result.Msg = "成功"
+		ed, edErr := time.Parse("2006-01-02 15:04:05", endDate)
+		if edErr != nil {
+			result.Code = -4
+			result.Msg = "预约结束时间不能格式不正确，必须是yyyy-MM-dd HH:mm:ss格式"
+		}
+		sd, sdErr := time.Parse("2006-01-02 15:04:05", startDate)
+		if sdErr != nil {
+			result.Code = -3
+			result.Msg = "预约开始时间不能格式不正确，必须是yyyy-MM-dd HH:mm:ss格式"
+		}
+		if edErr == nil && sdErr == nil {
+			m := entity.Meet{
+				Id:             bson.NewObjectId(),
+				City:           city,
+				Floor:          floor,
+				Room:           room,
+				ConferenceName: conferenceName,
+				People:         people,
+				Phone:          phone,
+				Password:       password,
+				StartDate:      sd,
+				EndDate:        ed,
+				Status:         true,
+				CreateDate:     time.Now(),
+			}
+			e := service.Reserve(m)
+			if e != nil {
+				result.Code = -100
+				result.Msg = e.Error()
+			} else {
+				result.Status = true
+				result.Msg = "成功"
+			}
+		}
 	}
 	json, _ := json.Marshal(result)
 	io.WriteString(w, string(json))
@@ -54,6 +73,7 @@ func Cancel(w http.ResponseWriter, r *http.Request) {
 	e := service.Cancel(id, password)
 	result := entity.Result{Status: false, Date: time.Now().Format("2006-01-02 15:04:05")}
 	if e != nil {
+		result.Code = -100
 		result.Msg = e.Error()
 	} else {
 		result.Status = true
@@ -71,39 +91,52 @@ func Query(w http.ResponseWriter, r *http.Request) {
 	endDate := r.FormValue("endDate")
 	var sd time.Time
 	var ed time.Time
+	var sdErr error
+	var edErr error
+	result := entity.Result{Status: false, Date: time.Now().Format("2006-01-02 15:04:05")}
 	if startDate != "" {
-		sd, _ = time.Parse("2006-01-02 15:04:05", startDate)
-	}
-	if endDate != "" {
-		ed, _ = time.Parse("2006-01-02 15:04:05", endDate)
-	}
-	ms, e := service.Query(city, floor, room, sd, ed)
-	jms := make([]entity.MeetJson, len(ms))
-	if ms != nil {
-		for i := 0; i < len(ms); i++ {
-			m := entity.MeetJson{
-				Id:             ms[i].Id.Hex(),
-				City:           ms[i].City,
-				Floor:          ms[i].Floor,
-				Room:           ms[i].Room,
-				ConferenceName: ms[i].ConferenceName,
-				People:         ms[i].People,
-				Phone:          ms[i].Phone,
-				StartDate:      ms[i].StartDate.Format("2006-01-02 15:04:05"),
-				EndDate:        ms[i].EndDate.Format("2006-01-02 15:04:05"),
-				Status:         ms[i].Status,
-				CreateDate:     ms[i].CreateDate.Format("2006-01-02 15:04:05"),
-			}
-			jms[i] = m
+		sd, sdErr = time.Parse("2006-01-02 15:04:05", startDate)
+		if sdErr != nil {
+			result.Code = -1
+			result.Msg = "开始时间不能格式不正确，必须是yyyy-MM-dd HH:mm:ss格式"
 		}
 	}
-	result := entity.Result{Status: false, Date: time.Now().Format("2006-01-02 15:04:05")}
-	if e != nil {
-		result.Msg = e.Error()
-	} else {
-		result.Status = true
-		result.Msg = "成功"
-		result.Data = jms
+	if endDate != "" {
+		ed, edErr = time.Parse("2006-01-02 15:04:05", endDate)
+		if edErr != nil {
+			result.Code = -2
+			result.Msg = "结束时间不能格式不正确，必须是yyyy-MM-dd HH:mm:ss格式"
+		}
+	}
+	if sdErr == nil && edErr == nil {
+		ms, e := service.Query(city, floor, room, sd, ed)
+		jms := make([]entity.MeetJson, len(ms))
+		if ms != nil {
+			for i := 0; i < len(ms); i++ {
+				m := entity.MeetJson{
+					Id:             ms[i].Id.Hex(),
+					City:           ms[i].City,
+					Floor:          ms[i].Floor,
+					Room:           ms[i].Room,
+					ConferenceName: ms[i].ConferenceName,
+					People:         ms[i].People,
+					Phone:          ms[i].Phone,
+					StartDate:      ms[i].StartDate.Format("2006-01-02 15:04:05"),
+					EndDate:        ms[i].EndDate.Format("2006-01-02 15:04:05"),
+					Status:         ms[i].Status,
+					CreateDate:     ms[i].CreateDate.Format("2006-01-02 15:04:05"),
+				}
+				jms[i] = m
+			}
+		}
+		if e != nil {
+			result.Code = -100
+			result.Msg = e.Error()
+		} else {
+			result.Status = true
+			result.Msg = "成功"
+			result.Data = jms
+		}
 	}
 	json, _ := json.Marshal(result)
 	io.WriteString(w, string(json))
